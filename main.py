7dad387b51fda2cfe384f9fe3b14851b80699712
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2023-12-05 20:12:26 +0500

    edit code and csv file

--- main.py
+++ main.py
@@ -7,36 +7,48 @@ class FileReader:
         self.folder_path = folder_path
 
     def read_files(self):
-        data = []
-        for filename in os.listdir(self.folder_path):
-            if filename.endswith(".txt"):
-                file_path = os.path.join(self.folder_path, filename)
-                with open(file_path, "r", encoding="utf-8") as file:
-                    lines = file.readlines()
-                    if len(lines) >= 3:
-                        name = lines[0].strip()
-                        price = lines[1].strip()
-                        description = lines[2].strip()
-                        data.append({"name": name, "price": price, "description": description})
-        return data
+        data_list = []
 
-    def write_to_csv(self, output_file="output.csv"):
-        data = self.read_files()
-        if data:
-            with open(output_file, "w", newline="", encoding="utf-8") as csv_file:
-                fieldnames = ["name", "price", "description"]
-                writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter="|")
-                writer.writeheader()
-                for row in data:
-                    writer.writerow(row)
-            print(f"Data has been written to {output_file} successfully.")
-        else:
-            print("No data to write.")
+        # folder_path ichidagi barcha fayllarni olish
+        files = [f for f in os.listdir(self.folder_path) if os.path.isfile(os.path.join(self.folder_path, f))]
+
+        for file_name in files:
+            file_path = os.path.join(self.folder_path, file_name)
+
+            # Faylni oqish
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+
+                # Fayl ichidagi ma'lumotlarni olish
+                name = lines[0].strip()
+                price = lines[1].strip()
+                description = lines[2].strip()
+
+                data_list.append({'name': name, 'price': price, 'description': description})
+
+        return data_list
+
+    def write_to_csv(self, csv_file_path, data_list):
+        # Ma'lumotlarni CSV fayliga yozish
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            fieldnames = ['name', 'price', 'description']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            # CSV faylning ustunlarini yozish
+            writer.writeheader()
+
+            # Ma'lumotlarni yozish
+            for data in data_list:
+                writer.writerow(data)
 
 
-# FileReader class'ini instansiyalash
-folder_path = "descriptions"
-file_reader = FileReader(folder_path)
+# FileReader klassini ishlatish
+if __name__ == "__main__":
+    folder_path = "descriptions"  # Ma'lumotlar uchun papkangizning nomi
+    csv_file_path = "output.csv"  # CSV fayli uchun nom
 
-# Ma'lumotlarni o'qish va CSV ga yozish
-file_reader.write_to_csv()
+    file_reader = FileReader(folder_path)
+    data_list = file_reader.read_files()
+
+    # CSV fayliga yozish
+    file_reader.write_to_csv(csv_file_path, data_list)
