12d9a95ad592172f4849e65323d2e7417bdcab3c
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2023-12-06 16:36:14 +0500

    Edit main file

--- main.py
+++ main.py
@@ -9,17 +9,14 @@ class FileReader:
     def read_files(self):
         data_list = []
 
-        # folder_path ichidagi barcha fayllarni olish
         files = [f for f in os.listdir(self.folder_path) if os.path.isfile(os.path.join(self.folder_path, f))]
 
         for file_name in files:
             file_path = os.path.join(self.folder_path, file_name)
 
-            # Faylni oqish
             with open(file_path, 'r') as file:
                 lines = file.readlines()
 
-                # Fayl ichidagi ma'lumotlarni olish
                 name = lines[0].strip()
                 price = lines[1].strip()
                 description = lines[2].strip()
@@ -29,20 +26,16 @@ class FileReader:
         return data_list
 
     def write_to_csv(self, csv_file_path, data_list):
-        # Ma'lumotlarni CSV fayliga yozish
         with open(csv_file_path, 'w', newline='') as csvfile:
             fieldnames = ['name', 'price', 'description']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-            # CSV faylning ustunlarini yozish
             writer.writeheader()
 
-            # Ma'lumotlarni yozish
             for data in data_list:
                 writer.writerow(data)
 
 
-# FileReader klassini ishlatish
 if __name__ == "__main__":
     folder_path = "descriptions"  # Ma'lumotlar uchun papkangizning nomi
     csv_file_path = "output.csv"  # CSV fayli uchun nom
@@ -50,5 +43,4 @@ if __name__ == "__main__":
     file_reader = FileReader(folder_path)
     data_list = file_reader.read_files()
 
-    # CSV fayliga yozish
     file_reader.write_to_csv(csv_file_path, data_list)
